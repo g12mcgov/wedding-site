f16eedf71903139f1e865231809abe69ec64b69e
Grant McGovern <grant@cobrain.com>
2022-04-21 19:25:35 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div
+        className="bg-cover bg-center h-screen flex"
+        style={{ backgroundImage: "url('proposal.jpg')" }}
+      >
+        <div className="m-auto">
+          <h1 className="text-9xl text-white text-center font-square-peg">
+            Megan & Grant
+          </h1>
+          <h2 className="text-6xl text-white text-center font-square-peg">
+            June 24, 2023
+          </h2>
+          <h2 className="text-6xl text-white text-center font-square-peg">
+            Cleveland, Ohio
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
