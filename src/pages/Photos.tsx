0a6fa5ffe981079896f273be63ea716e9aaff524
Grant McGovern <grant@cobrain.com>
2022-12-25 21:19:03 -0500

    Update credits

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -4,13 +4,13 @@ import { GalleryPreview } from "../components/collage/GalleryPreview";
 
 export const Photos = (): JSX.Element => {
   return (
-    <>
-      <div className="font-cormorant-garamond text-center">
+    <div className="font-cormorant-garamond">
+      <div className="text-center">
         <h1 className="mt-12 text-[#a0bbd3] sm:tracking-tight text-6xl sm:text-7xl">
           Photo Galleries
         </h1>
       </div>
-      <div className="flex flex-col lg:flex-row lg:space-x-8 my-16 px-8">
+      <div className="flex flex-col lg:flex-row lg:space-x-8 my-4 sm:my-16 px-4">
         <GalleryPreview
           name="engagement"
           description="Engagement"
@@ -27,6 +27,23 @@ export const Photos = (): JSX.Element => {
           image="/images/galleries/museum/139.webp"
         />
       </div>
-    </>
+      <div className="flex flex-col sm:flex-row items-center justify-center space-x-2 text-slate-300 mt-12 sm:mt-24 sm:mb-16 text-md">
+        <a
+          href="https://www.sparklephoto.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          2021 Sparkle Photography
+        </a>
+        <span className="hidden sm:block text-2xl">•</span>
+        <a
+          href="https://belindajeanphotography.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          2022 Belinda Jean Photography
+        </a>
+      </div>
+    </div>
   );
 };
